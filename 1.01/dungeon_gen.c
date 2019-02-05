afa3bd1d8d3a466abef259b3f5c5a48f619f3805
Chris <chrisjohannsen8@gmail.com>
2019-02-05 17:47:20 -0600

    Update 1.01

--- 1.01/dungeon_gen.c
+++ 1.01/dungeon_gen.c
@@ -20,7 +20,7 @@ int place_rooms(uint8_t num_rooms,
 {
 
   /* Initialize some things */
-  uint16_t i, j, k, isValid, max_height = 10, max_width = 10;
+  uint16_t i, j, r, c, isValid, max_height = 10, max_width = 20;
   srand(time(NULL));
 
   /*  
@@ -55,53 +55,129 @@ int place_rooms(uint8_t num_rooms,
       rooms[i].width = (rand() % (max_width-4)) + 4;
       rooms[i].height = (rand() % (max_height-3)) + 3;
 
-      /* Place and check validity */
-      isValid = 1, j = rooms[i].y-1;
-      do{
-	k = rooms[i].x-1;
-	do{
-	  if(map[j][k] == 0){
-	    k--;
-	    for(j = j; j > rooms[i].y-1; j--){
-	      for(k = k; k > rooms[i].x-1; k--){
-		map[j][k] = 1;
-	      }
-	    }
-	    isValid = 0;
-	  }else if(k != rooms[i].x-1 && k != rooms[i].x+rooms[i].width+1 &&
-		   j != rooms[i].y-1 && j != rooms[i].y+rooms[i].height+1){
-	    map[j][k] = 0;
-	  }
-	  k++;
-	}while(isValid && k < rooms[i].width+rooms[i].x+1);
-	j++;
-      }while(isValid && j < rooms[i].height+rooms[i].y+1);
+      isValid = 1;
+
+      for(r = rooms[i].y-1; r < rooms[i].y+rooms[i].height+1; r++){
+	for(c = rooms[i].x-1; c < rooms[i].x+rooms[i].width+1; c++){
+	  if(map[r][c] == 0 || map[r][c] == 255){ isValid = 0; }
+	}
+      }
     }while(!isValid);
+
+    /* Place room */
+    for(r = rooms[i].y; r < rooms[i].y+rooms[i].height; r++){
+      for(c = rooms[i].x; c < rooms[i].x+rooms[i].width; c++){
+	map[r][c] = 0;
+      }
+    }
   }
 
   return 0;
 }
 
-int place_corridors(uint8_t map[21][80]){
+int place_corridors(uint8_t num_rooms, 
+		    struct room rooms[],
+		    uint8_t map[21][80]){
+
+  uint16_t i, r, c, room1, room2;
+
+  for(i = 0; i < num_rooms; i++){
+    room1 = i;
+    room2 = (i + 1) % num_rooms;
+
+    r = rooms[room1].y;
+    c = rooms[room1].x;
+    
+    do{
+      if(r < rooms[room2].y){ r++; }
+      else if(r > rooms[room2].y){ r--; }
+      else if(c < rooms[room2].x){ c++; }
+      else if(c > rooms[room2].x){ c--; }
+      
+      if(map[r][c] != 255){ map[r][c] = 0; }
+    }while( r != rooms[room2].y || c != rooms[room2].x);
+  }
+
+  return 0;
+}
+
+int place_stairs(uint8_t stairs[],
+		 uint8_t num_rooms,
+		 struct room rooms[],
+		 uint8_t map[21][80]){
+
+  uint16_t rand_room, rand_row, rand_col;
+  srand(time(NULL));
+
+  rand_room = rand() % num_rooms;
+  rand_row = (rand() % rooms[rand_room].height) + rooms[rand_room].y;
+  rand_col = (rand() % rooms[rand_room].width) + rooms[rand_room].x;
+  stairs[0] = 0;
+  stairs[1] = rand_row;
+  stairs[2] = rand_col;
+
+  do{
+    rand_room = rand() % num_rooms;
+    rand_row = (rand() % rooms[rand_room].height) + rooms[rand_room].y;
+    rand_col = (rand() % rooms[rand_room].width) + rooms[rand_room].x;
+    stairs[3] = 1;
+    stairs[4] = rand_row;
+    stairs[5] = rand_col;
+  }while(stairs[1] == stairs[4] || stairs[2] == stairs[5]);
+
   return 0;
 }
 
 int main(int argc, char *argv[]){
 
-  uint8_t dungeon[DUNGEON_HEIGHT][DUNGEON_WIDTH];
-  struct room rooms[6];
+  uint8_t dungeon[DUNGEON_HEIGHT][DUNGEON_WIDTH], num_rooms = 6;
+  uint8_t stairs[6];
+  struct room rooms[num_rooms];
 
-  place_rooms(6, rooms, dungeon);
-  place_corridors(dungeon);
+  place_rooms(num_rooms, rooms, dungeon);
+  place_corridors(num_rooms, rooms, dungeon);
+  place_stairs(stairs, num_rooms, rooms, dungeon);
   
-  int r, c;
-  for(r = 0; r < DUNGEON_HEIGHT; r ++){
+  char char_dungeon[DUNGEON_HEIGHT][DUNGEON_WIDTH];
+  int r, c, i;
+  for(r = 0; r < DUNGEON_HEIGHT; r++){
     for(c = 0; c < DUNGEON_WIDTH; c++){
-      if(dungeon[r][c] > 0){
-	printf(" ");
-      }else{
-	printf(".");
+      char_dungeon[r][c] = ' ';
+    }
+  }
+
+  for(i = 0; i < num_rooms; i++){
+    for(r = rooms[i].y; r < rooms[i].y+rooms[i].height; r++){
+      for(c = rooms[i].x; c < rooms[i].x+rooms[i].width; c++){
+	char_dungeon[r][c] = '.';
       }
+    }
+  }
+
+  for(i = 0; i < 6; i=i+3){
+    if(stairs[i] == 0){
+      char_dungeon[stairs[i+1]][stairs[i+2]] = '>';
+    }else{
+      char_dungeon[stairs[i+1]][stairs[i+2]] = '<';
+    }
+  }
+
+  for(r = 0; r < DUNGEON_HEIGHT; r++){
+    for(c = 0; c < DUNGEON_WIDTH; c++){
+      if(char_dungeon[r][c] != '.' && 
+	char_dungeon[r][c] != '>' &&
+	char_dungeon[r][c] != '<'){
+	if(dungeon[r][c] > 0){ char_dungeon[r][c] = ' '; }
+	else{ char_dungeon[r][c] = '#'; }
+      }
+    }
+  }
+
+  printf("%c\n", char_dungeon[20][2]);
+
+  for(r = 0; r < DUNGEON_HEIGHT; r++){
+    for(c = 0; c < DUNGEON_WIDTH; c++){
+      printf("%c", char_dungeon[r][c]);
     }
     printf("\n");
   }
